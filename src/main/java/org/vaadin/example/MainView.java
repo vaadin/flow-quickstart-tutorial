e279030d1b78546973d40a27f2d38c34d1984932
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2022-02-25 10:15:45 +0200

    Initial commit

---
+++ src/main/java/org/vaadin/example/MainView.java
@@ -0,0 +1,34 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        VerticalLayout todosList = new VerticalLayout();
+        TextField taskField = new TextField();
+        Button addButton = new Button("Add");
+        addButton.addClickListener(click -> {
+            Checkbox checkbox = new Checkbox(taskField.getValue());
+            todosList.add(checkbox);
+        });
+        addButton.addClickShortcut(Key.ENTER);
+
+        add(
+                new H1("Vaadin Todo"),
+                todosList,
+                new HorizontalLayout(
+                        taskField,
+                        addButton
+                )
+        );
+    }
+}
