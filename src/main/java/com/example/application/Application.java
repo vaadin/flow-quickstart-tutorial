21fe2db8dedb9dc2ebd848dc1d416120b1e1d153
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2023-04-26 11:15:57 +0300

    Update to 24.0.4 and cleanup (#69)

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -18,7 +18,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 @Theme(value = "mytodo")
 @PWA(name = "My Todo", shortName = "My Todo", offlineResources = {"images/logo.png"})
-@NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
